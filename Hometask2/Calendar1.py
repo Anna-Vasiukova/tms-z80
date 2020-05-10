94ed681730b64142b74377b058c7afed0f108c4c
Anna-Vasiukova <vasiukova.ann@yandex.ru>
2020-05-10 17:37:28 +0300

    Hometask 2

--- Hometask2/Calendar1.py
+++ Hometask2/Calendar1.py
@@ -1,20 +1,5 @@
-import calendar
+from calendar import weekday, day_name
 s = input()
 x = s.split('.')
-a = calendar.weekday(int(x[-1]), int(x[-2]), int(x[-3]))
-if a == 0:
-    print('Monday')
-elif a == 1:
-    print('Tuesday')
-elif a == 2:
-    print('Wednesday')
-elif a == 3:
-    print('Thursday')
-elif a == 4:
-    print('Friday')
-elif a == 5:
-    print('Saturday')
-elif a == 6:
-    print('Sunday')
-else:
-    print('Error')
+a = weekday(int(x[-1]), int(x[-2]), int(x[-3]))
+print(day_name[a])
